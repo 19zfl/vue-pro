cf4a70b76a77d1ed23f73f68d35ef7cd861463bd
zfl19 <19aleiya@gmail.com>
2024-03-27 21:31:58 +0800

    add TextView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <TextView msg="这是自定义的组件TextView"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TextView from './components/TextView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TextView
   }
 }
 </script>
---
+++ src/components/TextView.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'TextView',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
