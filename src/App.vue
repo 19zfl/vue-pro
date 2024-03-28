3511d2b2b0d6f48afbcfeb9932bd009ff6fc78f5
zfl19 <19aleiya@gmail.com>
2024-03-28 14:34:20 +0800

    add router config

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <TextView msg="这是自定义的组件TextView"/>
+  <!-- <TextView msg="这是自定义的组件TextView"/> -->
+  <div class="app-container">
+    <h1>App根组件</h1>
+    <router-link to="/home">首页</router-link>
+    <router-link to="/about">关于</router-link>
+    <br>
+    <router-view></router-view>
+  </div>
 </template>
 
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
+<!-- <script>
+import HelloWorld from './components/HelloWorld.vue'
 import TextView from './components/TextView.vue'
 
 export default {
   name: 'App',
   components: {
-    TextView
   }
 }
-</script>
+</script> -->
+<style scoped>
+.app-container {
+  text-align: center;
+  font-size: 16px;
+}
+.app-container a {
+  padding: 10px;
+  color: #000;
+}
+.app-container a.router-link-active {
+  color: #fff;
+  background-color: #000;
+}
+</style>
 
-<style>
+<!-- <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +45,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
